564d5c3d7076a1ae274c21daa21e373979d5cfaa
Farshidroohi <farshid.roohi.a@gmail.com>
2021-05-17 13:44:17 +0430

    update

---
+++ PrivateTest.framework/Headers/PrivateTest.h
@@ -0,0 +1,18 @@
+//
+//  PrivateTest.h
+//  PrivateTest
+//
+//  Created by Farshid on 5/17/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrivateTest.
+FOUNDATION_EXPORT double PrivateTestVersionNumber;
+
+//! Project version string for PrivateTest.
+FOUNDATION_EXPORT const unsigned char PrivateTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivateTest/PublicHeader.h>
+
+
